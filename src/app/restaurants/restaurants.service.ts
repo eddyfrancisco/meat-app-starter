219c5c0a187020f684752af7a9756e9f79c02e11
root <edgenilson.silva@sbkbpo.com.br>
2018-02-23 17:04:18 -0300

    Até a ula 46

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -5,8 +5,10 @@ import {Observable} from 'rxjs/Observable'
 import {Restaurant} from './restaurant/restaurant.model'
 
 import {MEAT_API} from '../app.api'
+import {ErrorHandler} from '../app.error-handler'
 
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
 
 @Injectable()
 
@@ -17,6 +19,7 @@ export class RestaurantsService {
       restaurants(): Observable<Restaurant[]>{
           return this.http.get(`${MEAT_API}/restaurants`)
           .map(response => response.json() )
+          .catch(ErrorHandler.handleError)
       }
 
 }
