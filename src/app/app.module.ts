df41bdf2050e0632441a4d21a45b6b33b08bcd7d
eddyfrancisco <eddy.pegasus@gmail.com>
2018-06-28 00:34:35 -0300

    Até aula 80

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,15 +11,14 @@ import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
-import { RestaurantsService  } from './restaurants/restaurants.service';
+
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
 import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
-import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart-service';
 
-import { OrderService } from './order/order.service';
+
 import { OrderSummaryComponent } from './order-summary/order-summary.component';
 import { SharedModule } from './shared/shared.module';
 
@@ -42,10 +41,10 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     HttpModule,
-    SharedModule,
+    SharedModule.forRoot(),
     RouterModule.forRoot(ROUTES)
   ],
-  providers: [RestaurantsService, ShoppingCartService, OrderService, {provide: LOCALE_ID, useValue: 'pt-BR'}],
+  providers: [ {provide: LOCALE_ID, useValue: 'pt-BR'}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from "@angular/core";
+import { NgModule, ModuleWithProviders } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { InputComponent } from './input/input.component';
 import { RatingComponent } from './rating/rating.component';
 import { RadioComponent } from './radio/radio.component';
+
+import { ShoppingCartService } from '../restaurant-detail/shopping-cart/shopping-cart-service';
+import { RestaurantsService } from '../restaurants/restaurants.service';
+import { OrderService } from '../order/order.service';
 
 @NgModule({
 
@@ -14,4 +18,11 @@ import { RadioComponent } from './radio/radio.component';
         CommonModule, FormsModule, ReactiveFormsModule]
 
 })
-export class SharedModule { };
+export class SharedModule { 
+    static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: SharedModule,
+            providers: [ShoppingCartService, RestaurantsService, OrderService ]
+        }
+    }
+};
